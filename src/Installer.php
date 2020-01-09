373d0f0a0eb1cce4eae414edd0d6f627e31bd476
henrygotmojo <henrygotmojo@gmail.com>
2020-01-09 12:47:57 +0800

    create empty installer and plugin scripts

---
+++ src/Installer.php
@@ -0,0 +1,34 @@
+<?php
+namespace Fuseboxy\Composer;
+
+use Composer\Installer\InstallerInterface;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+
+class Plugin implements InstallerInterface {
+
+	public function supports($packageType) {
+
+	}
+
+	public function isInstalled(InstalledRepositoryInterface $repo, PackageInterface $package) {
+
+	}
+
+	public function install(InstalledRepositoryInterface $repo, PackageInterface $package) {
+
+	}
+
+	public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target) {
+
+	}
+
+	public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package) {
+
+	}
+
+	public function getInstallPath(PackageInterface $package) {
+
+	}
+
+}
---
+++ src/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+namespace Fuseboxy\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface {
+
+	public function activate(Composer $composer, IOInterface $io) {
+		
+	}
+
+}
