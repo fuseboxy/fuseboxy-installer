d6ecd787c4c0890742dd622e9ab7c9a60c94cacc
henrygotmojo <henrygotmojo@gmail.com>
2020-05-26 17:59:12 +0800

    fallback to user Helper component because the program cannot recognize rrmdir as a global function

---
+++ src/Helper.php
@@ -0,0 +1,19 @@
+<?php
+namespace Fuseboxy\Composer;
+
+
+class Helper {
+
+	// recursively remove a folder with contents (or simply remove a file)
+	public static function rrmdir($dir) {
+		if ( is_dir($dir) ) {
+			$objects = scandir($dir);
+			foreach ( $objects as $obj ) if ( !in_array($obj, ['.','..']) ) rrmdir($dir.'/'.$obj);
+			rmdir($dir);
+		} elseif ( is_file($dir) or is_link($dir) ) {
+			unlink($dir);
+		}
+		return true;
+	}
+
+} // class
