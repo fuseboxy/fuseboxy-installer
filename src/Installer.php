7f427badd03dc826e2c209a9796a16019f6e6fcb
henrygotmojo <henrygotmojo@gmail.com>
2020-05-29 16:38:14 +0800

    fix bug

--- src/Installer.php
+++ src/Installer.php
@@ -32,19 +32,19 @@ class Installer extends LibraryInstaller {
 		$baseDir = dirname($this->vendorDir).'/';
 		$packageDir = $this->vendorDir.'/'.$package->getName().'/';
 		// create directories
-		foreach ( self::$file2move[$package->getType()] as $file ) {
+		foreach ( self::$file2move as $file ) {
 			$dir = dirname($file);
 			if ( $dir != '.' and !is_dir($baseDir.$dir) ) mkdir($baseDir.$dir, 0755, true);
 		}
 		// copy files
 		// ===> do not overwrite!
 		// ===> otherwise, modified config file or customized index will be overwritten everytime composer update is run
-		foreach ( self::$file2move[$package->getType()] as $file ) {
+		foreach ( self::$file2move as $file ) {
 			if ( !is_file($baseDir.$file) ) copy($packageDir.$file, $baseDir.$file);
 		}
 		// remove copied files
 		// ===> so that only core files (but not config file) remain in vendor directory
-		foreach ( self::$file2move[$package->getType()] as $file ) Helper::rrmdir($packageDir.$file);
+		foreach ( self::$file2move as $file ) Helper::rrmdir($packageDir.$file);
 		// done!
 		return true;
 	}
