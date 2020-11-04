d9745a5c0c07d348f5ac36d2f4cb590ff783733c
henrygotmojo <henrygotmojo@gmail.com>
2020-11-04 15:05:14 +0800

    do not copy certain package files to base-dir when unit test

--- src/Installer.php
+++ src/Installer.php
@@ -28,6 +28,14 @@ class Installer extends LibraryInstaller {
 	);
 
 
+	private function isUnitTest() {
+		$filePath = dirname($this->vendorDir).'/composer.json';
+		$json = json_decode(file_get_contents($filePath), true);
+		if ( $json === false ) return false;
+		return ( isset($json['name']) and $json['name'] == 'fuseboxy/fuseboxy-test' );
+	}
+
+
 	public function supports($packageType) {
 		return in_array($packageType, ['fuseboxy-core', 'fuseboxy-module']);
 	}
@@ -39,21 +47,24 @@ class Installer extends LibraryInstaller {
 		// define target and source directories
 		$baseDir = dirname($this->vendorDir).'/';
 		$packageDir = $this->vendorDir.'/'.$package->getName().'/';
-		// create directories
-		foreach ( self::$file2move[$package->getType()] as $file ) {
-			$dir = dirname($file);
-			if ( $dir != '.' and !is_dir($baseDir.$dir) ) mkdir($baseDir.$dir, 0755, true);
+		// further adjust package location (when necessary)
+		if ( !$this->isUnitTest() ) {
+			// create directories
+			foreach ( self::$file2move[$package->getType()] as $file ) {
+				$dir = dirname($file);
+				if ( $dir != '.' and !is_dir($baseDir.$dir) ) mkdir($baseDir.$dir, 0755, true);
+			}
+			// copy files
+			// ===> do not overwrite!
+			// ===> otherwise, modified config file or customized index will be overwritten everytime composer update is run
+			foreach ( self::$file2move[$package->getType()] as $file ) if ( !is_file($baseDir.$file) ) copy($packageDir.$file, $baseDir.$file);
+			// remove copied files
+			// ===> so that only core files (but not config file) remain in vendor directory
+			foreach ( self::$file2move[$package->getType()] as $file ) Helper::rrmdir($packageDir.$file);
+			// remove certain directories
+			// ===> so that git will put fuseboxy stuff into repo (instead of considering them as submodules)
+			foreach ( self::$dir2remove[$package->getType()] as $dir ) Helper::rrmdir($packageDir.$dir);
 		}
-		// copy files
-		// ===> do not overwrite!
-		// ===> otherwise, modified config file or customized index will be overwritten everytime composer update is run
-		foreach ( self::$file2move[$package->getType()] as $file ) if ( !is_file($baseDir.$file) ) copy($packageDir.$file, $baseDir.$file);
-		// remove copied files
-		// ===> so that only core files (but not config file) remain in vendor directory
-		foreach ( self::$file2move[$package->getType()] as $file ) Helper::rrmdir($packageDir.$file);
-		// remove certain directories
-		// ===> so that git will put fuseboxy stuff into repo (instead of considering them as submodules)
-		foreach ( self::$dir2remove[$package->getType()] as $dir ) Helper::rrmdir($packageDir.$dir);
 		// done!
 		return true;
 	}
@@ -64,12 +75,15 @@ class Installer extends LibraryInstaller {
 		parent::update($repo, $initial, $target);
 		// define target directory
 		$packageDir = $this->vendorDir.'/'.$target->getName().'/';
-		// remove files that already copied in installation
-		// ===> so that only core files (but not config file) remain in vendor directory
-		foreach ( self::$file2move[$target->getType()] as $file ) Helper::rrmdir($packageDir.$file);
-		// remove certain directories
-		// ===> so that git will put fuseboxy stuff into repo (instead of considering them as submodules)
-		foreach ( self::$dir2remove[$target->getType()] as $dir ) Helper::rrmdir($packageDir.$dir);
+		// further adjust package location (when necessary)
+		if ( !$this->isUnitTest() ) {
+			// remove files that already copied in installation
+			// ===> so that only core files (but not config file) remain in vendor directory
+			foreach ( self::$file2move[$target->getType()] as $file ) Helper::rrmdir($packageDir.$file);
+			// remove certain directories
+			// ===> so that git will put fuseboxy stuff into repo (instead of considering them as submodules)
+			foreach ( self::$dir2remove[$target->getType()] as $dir ) Helper::rrmdir($packageDir.$dir);
+		}
 		// done!
 		return true;
 	}
